6944706ce78e51078e8277f96aa5e6b1a2908288
chetantarale <chetan@shippable.com>
2016-03-31 17:37:06 +0530

    Update MemcachedExampleTest.php

--- tests/MemcachedExampleTest.php
+++ tests/MemcachedExampleTest.php
@@ -3,8 +3,6 @@ require_once('MemcachedExample.php');
 class MemcachedExampleTest extends PHPUnit_Framework_TestCase {
   public function testMemcached() {
   	$mem = new MemcachedExample();
-  	echo "hello";
-  	echo $mem;
     $this->assertEquals($mem->populate(), 99);
   }
 }
